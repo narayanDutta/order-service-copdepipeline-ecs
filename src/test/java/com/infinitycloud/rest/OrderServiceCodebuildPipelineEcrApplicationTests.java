8e74af3426d73a61abcaba61e7c622c5cc217dc8
Narayan <narayandutta85@gmail.com>
2020-10-30 11:25:56 +0530

    myfirst commit

---
+++ src/test/java/com/infinitycloud/rest/OrderServiceCodebuildPipelineEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitycloud.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceCodebuildPipelineEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
